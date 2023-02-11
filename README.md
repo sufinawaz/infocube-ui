82dcd819faa80c6038fbeea14db9ed057b740ea1
Sufi Nawaz <sufi@eaiti.com>
2023-02-11 00:14:24 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# React App to Control InfoCube Display Unit
+
+## Available Scripts
+
+In the project directory, you can run:
+
+### `npm start`
+
+Runs the app in the development mode.\
+Open [http://localhost:3001](http://localhost:3001) to view it in the browser.
+
+The page will reload if you make edits.\
+You will also see any lint errors in the console.
+
+### `npm build`
+
+Builds the app for production to the `build` folder.\
+It correctly bundles React in production mode and optimizes the build for the best performance.
---
+++ src/App.js
@@ -0,0 +1,171 @@
+import React, { createContext, useEffect, useState } from 'react';
+import './App.css';
+import { Card, Form, Input, Row, Col, Button, Space, List } from 'antd';
+import {
+  ClockCircleOutlined,
+  FireOutlined,
+  DotChartOutlined,
+  MessageOutlined,
+  CalendarOutlined,
+} from '@ant-design/icons';
+import mqtt from 'mqtt/dist/mqtt';
+console.log('process.env', process.env);
+export const QosOption = createContext([]);
+const qosOption = [
+  {
+    label: '0',
+    value: 0,
+  },
+  {
+    label: '1',
+    value: 1,
+  },
+  {
+    label: '2',
+    value: 2,
+  },
+];
+function App() {
+  const [form] = Form.useForm();
+
+  const onFinish = (values) => {
+    publish(values);
+  };
+  const quotes = ['Hello Beautiful!', 'Well, hello there!', "Hi Goodlookin'!"];
+  const options = {
+    AQI: { title: 'Air Quality', Icon: DotChartOutlined },
+    prayer: { title: 'Prayer Times', Icon: CalendarOutlined },
+    clock: { title: 'Clock', Icon: ClockCircleOutlined },
+    fireplace: { title: 'Fireplace', Icon: FireOutlined },
+    random: { title: 'Random Boost', Icon: MessageOutlined },
+  };
+  const PublishForm = (
+    <Form layout='vertical' name='basic' form={form} onFinish={onFinish}>
+      <Row>
+        <Col>
+          <Form.Item label='' name='payload'>
+            <Input.TextArea />
+          </Form.Item>
+        </Col>
+      </Row>
+      <Row>
+        <Col className='ant-col'>
+          <Form.Item>
+            <Button type='primary' htmlType='submit'>
+              Publish
+            </Button>
+          </Form.Item>
+        </Col>
+      </Row>
+    </Form>
+  );
+  const [client, setClient] = useState(null);
+
+  const mqttConnect = (host, mqttOption) => {
+    setClient(mqtt.connect(host, mqttOption));
+  };
+
+  const record = {
+    host: process.env.REACT_APP_MQTT_URL,
+    clientId: `mqttjs_ + ${Math.random()
+      .toString(16)
+      .substr(2, 8)}`,
+    port: process.env.REACT_APP_MQTT_PORT,
+    topic: process.env.REACT_APP_MQTT_TOPIC,
+    qos: 2,
+  };
+  const connectToMqtt = () => {
+    const { host, clientId, port } = record;
+    const url = `ws://${host}:${port}`;
+    const options = {
+      keepalive: 30,
+      protocolId: 'MQTT',
+      protocolVersion: 4,
+      clean: true,
+      reconnectPeriod: 1000,
+      connectTimeout: 30 * 1000,
+      rejectUnauthorized: false,
+    };
+    options.clientId = clientId;
+    mqttConnect(url, options);
+  };
+
+  useEffect(() => {
+    connectToMqtt();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []); // connect once at startup
+  useEffect(() => {
+    if (client) {
+      client.on('error', (err) => {
+        console.error('Connection error: ', err);
+        client.end();
+      });
+    }
+  }, [client]);
+
+  const publish = (context) => {
+    if (client) {
+      const { payload } = context;
+      client.publish(record.topic, payload, { qos: record.qos }, (error) => {
+        if (error) {
+          console.log('Publish error: ', error);
+        }
+      });
+    }
+  };
+  return (
+    <div className='App' style={{ margin: 20, padding: 20 }}>
+      {/* <MQTTConnect /> */}
+      <QosOption.Provider value={qosOption}>
+        <div className='site-card-wrapper'>
+          <Space wrap>
+            <List
+              size='large'
+              // header={<div>Display Options</div>}
+              dataSource={Object.entries(options)}
+              renderItem={(value) => {
+                const Icon = value[1].Icon;
+                const key = value[0];
+                return (
+                  <List.Item>
+                    <Button
+                      block={true}
+                      size='large'
+                      type='primary'
+                      onClick={() =>
+                        publish({
+                          payload:
+                            key === 'random'
+                              ? quotes[
+                                  Math.floor(Math.random() * quotes.length)
+                                ]
+                              : key,
+                          // value:
+                          //   key === 'random'
+                          //     ? quotes[
+                          //         Math.floor(Math.random() * quotes.length)
+                          //       ]
+                          //     : `value: ${key}`,
+                        })
+                      }
+                    >
+                      {<Icon />}
+                      {value[1].title}
+                    </Button>
+                  </List.Item>
+                );
+              }}
+            />
+            <Col span={20}>
+              <Card title='Custom Text' bordered={false}>
+                {PublishForm}
+              </Card>
+            </Col>
+          </Space>
+        </div>
+      </QosOption.Provider>
+    </div>
+  );
+}
+
+export default App;
