4bf8756b43f2706c58c8c72688575fc3a53f5f79
Sufi Nawaz <sufi@eaiti.com>
2023-02-14 22:02:19 -0500

    Dropdown for gifs

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { createContext, useEffect, useState } from 'react';
 import './App.css';
-import { Card, Form, Input, Row, Col, Button, Space, List } from 'antd';
+import { Card, Form, Input, Row, Col, Button, Space, List, Select } from 'antd';
 import {
   ClockCircleOutlined,
-  FireOutlined,
   DotChartOutlined,
   MessageOutlined,
   CalendarOutlined,
+  PlayCircleOutlined,
 } from '@ant-design/icons';
 import mqtt from 'mqtt/dist/mqtt';
 export const QosOption = createContext([]);
@@ -26,17 +26,27 @@ const qosOption = [
 ];
 function App() {
   const [form] = Form.useForm();
+  const [gifOption, setGifOption] = useState(null);
 
   const onFinish = (values) => {
     publish(values);
   };
   const quotes = ['Hello Beautiful!', 'Well, hello there!', "Hi Goodlookin'!"];
   const options = {
-    AQI: { title: 'Air Quality', Icon: DotChartOutlined },
-    prayer: { title: 'Prayer Times', Icon: CalendarOutlined },
-    clock: { title: 'Clock', Icon: ClockCircleOutlined },
-    fireplace: { title: 'Fireplace', Icon: FireOutlined },
-    random: { title: 'Random Boost', Icon: MessageOutlined },
+    AQI: { title: 'Air Quality', Icon: DotChartOutlined, type: 'button' },
+    prayer: { title: 'Prayer Times', Icon: CalendarOutlined, type: 'button' },
+    gif: {
+      title: 'Play GIF',
+      Icon: PlayCircleOutlined,
+      options: [
+        { value: 'fireplace', label: 'Fireplace' },
+        { value: 'matrix', label: 'Matrix' },
+        { value: 'retro', label: 'Retro Vibe' },
+      ],
+      type: 'select',
+    },
+    clock: { title: 'Clock', Icon: ClockCircleOutlined, type: 'button' },
+    random: { title: 'Random Boost', Icon: MessageOutlined, type: 'button' },
   };
   const PublishForm = (
     <Form layout='vertical' name='basic' form={form} onFinish={onFinish}>
@@ -127,37 +137,60 @@ function App() {
               size='large'
               // header={<div>Display Options</div>}
               dataSource={Object.entries(options)}
-              renderItem={(value) => {
-                const Icon = value[1].Icon;
-                const key = value[0];
-                return (
-                  <List.Item>
-                    <Button
-                      block={true}
-                      size='large'
-                      type='primary'
-                      onClick={() =>
-                        publish({
-                          payload:
-                            key === 'random'
-                              ? quotes[
-                                  Math.floor(Math.random() * quotes.length)
-                                ]
-                              : key,
-                          // value:
-                          //   key === 'random'
-                          //     ? quotes[
-                          //         Math.floor(Math.random() * quotes.length)
-                          //       ]
-                          //     : `value: ${key}`,
-                        })
-                      }
-                    >
-                      {<Icon />}
-                      {value[1].title}
-                    </Button>
-                  </List.Item>
-                );
+              renderItem={(option) => {
+                const value = option[1];
+                const Icon = value.Icon;
+                const key = option[0];
+                switch (value.type) {
+                  case 'button':
+                    return (
+                      <List.Item>
+                        <Button
+                          block={true}
+                          size='large'
+                          type='primary'
+                          onClick={() =>
+                            publish({
+                              payload:
+                                key === 'random'
+                                  ? quotes[
+                                      Math.floor(Math.random() * quotes.length)
+                                    ]
+                                  : key,
+                            })
+                          }
+                        >
+                          {<Icon />}
+                          {value.title}
+                        </Button>
+                      </List.Item>
+                    );
+                  case 'select':
+                    return (
+                      <List.Item>
+                        <Select
+                          defaultValue='fireplace'
+                          style={{ width: 120 }}
+                          onChange={(val) => setGifOption(val)}
+                          options={value.options}
+                        />
+                        <Button
+                          type='primary'
+                          onClick={() => {
+                            if (gifOption !== null) {
+                              publish({
+                                payload: gifOption,
+                              });
+                            }
+                          }}
+                        >
+                          <PlayCircleOutlined />
+                        </Button>
+                      </List.Item>
+                    );
+                  default:
+                    break;
+                }
               }}
             />
             <Col span={20}>
